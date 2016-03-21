f3620f4612dd1bac9b0ce0a3260ee3a517afc81d
xiangchao027 <xiangchao027@gmail.com>
2016-03-21 19:13:56 +0800

    first commit

---
+++ hlnn.py
@@ -0,0 +1,9 @@
+# HLNN.py
+# class of Hybrid Learning Neural Network
+
+class HLNN :
+	def __init__(self, \
+output_layer_dim,\
+begin_neighbor_scope,\
+begin_learning_rate,\
+
---
+++ main.py
@@ -0,0 +1,7 @@
+# procedure for starting the application of instance of HLNN
+def main() :
+	print 'HLNN Instance Running Test'
+	net = HLNN();
+	
+main()
+	
