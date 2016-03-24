f657fb35a8dba6ff637f3e8b30d81c614a0988c1
xiangchao027 <xiangchao027@gmail.com>
2016-03-24 11:24:59 +0800

    update HLNN initlization method

--- hlnn.py
+++ hlnn.py
@@ -2,8 +2,25 @@
 # class of Hybrid Learning Neural Network
 
 class HLNN :
+	"
+	som_dim: dimensionality of SOM
+	som_eta: learning rate of SOM
+	som_rad: neighborhood radius
+	som_dec: decrease rate of correctness
+	bp_layer: layer number of BPNN
+	bp_dim: dimensionalities of all BPNN layers
+	bp_eta: BPNN learning reate
+	bp_coe: BPNN learning rate coefficient as cur*coe+last*(1-coe)
+	"
+
 	def __init__(self, \
-output_layer_dim,\
-begin_neighbor_scope,\
-begin_learning_rate,\
+	som_dim=10, \
+	som_eta=0.2, \
+	som_rad=3, \
+	som_dec=0.5, \
+	bp_layer=1, \
+	bp_dim=[1], \
+	bp_eta=0.6, \
+	bp_coe=0.5) :
+		print som_dim
 
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # procedure for starting the application of instance of HLNN
+import HLNN
+
 def main() :
 	print 'HLNN Instance Running Test'
 	net = HLNN();
