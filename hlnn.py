0b084a872bbf7e334a1a8e520b38eeb789482500
xiangchao027 <xiangchao027@gmail.com>
2016-03-24 13:06:10 +0800

    update model implementation

--- hlnn.py
+++ hlnn.py
@@ -1,26 +1,40 @@
 # HLNN.py
 # class of Hybrid Learning Neural Network
 
-class HLNN :
-	"
-	som_dim: dimensionality of SOM
-	som_eta: learning rate of SOM
-	som_rad: neighborhood radius
-	som_dec: decrease rate of correctness
-	bp_layer: layer number of BPNN
-	bp_dim: dimensionalities of all BPNN layers
-	bp_eta: BPNN learning reate
-	bp_coe: BPNN learning rate coefficient as cur*coe+last*(1-coe)
-	"
+# class param definition
+#	som_dim: dimensionality of SOM
+#	som_eta: learning rate of SOM
+#	som_rad: neighborhood radius
+#	som_dec: decrease rate of correctness
+#	bp_layer: layer number of BPNN
+#	bp_dim: dimensionalities of all BPNN layers
+#	bp_eta: BPNN learning reate
+#	bp_coe: BPNN learning rate coefficient as cur*coe+last*(1-coe)
 
-	def __init__(self, \
-	som_dim=10, \
-	som_eta=0.2, \
-	som_rad=3, \
-	som_dec=0.5, \
-	bp_layer=1, \
-	bp_dim=[1], \
-	bp_eta=0.6, \
-	bp_coe=0.5) :
-		print som_dim
 
+class HLNN:
+	def set_model(self, som_dim, som_eta, som_rad, som_dec, bp_layer, bp_dim, bp_eta, bp_coe):
+		self.som_dim = som_dim
+		self.som_eta = som_eta
+		self.som_rad = som_rad
+		self.som_dec = som_dec
+		self.bp_layer = bp_layer
+		self.bp_dim = bp_dim
+		self.bp_eta = bp_eta
+		self.bp_coe = bp_coe
+	def set_som_dim(self, som_dim):
+		self.som_dim = som_dim
+	def set_som_eta(self, som_eta):
+		self.som_eta = som_eta
+	def set_som_rad(self, som_rad):
+		self.som_rad = som_rad
+	def set_som_dec(self, som_dec):
+		self.som_dec = som_dec
+	def set_bp_layer(self, bp_layer):
+		self.bp_layer = bp_layer
+	def set_bp_dim(self, bp_dim):
+		self.bp_dim = bp_dim
+	def set_bp_eta(self, bp_eta):
+		self.bp_eta = bp_eta
+	def set_bp_coe(self, bp_coe):
+		self.bp_coe = bp_coe
