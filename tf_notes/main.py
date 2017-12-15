remain a bug:energy computation

tf_notes/main.py
@@ -6,24 +6,16 @@ import numpy as np
 import os
 import sys
 
+import tf_hlconvnet as hl
+
 # constants for net trival settings
-N_CLASS = 28    # number of classes
-X_FEATURE = 'x' # name of input feature
 
-def make_hybrid_learning_net(features, labels, mode):
-    ''' Building a hybrid learning net'''
-    x = features[X_FEATURE]
-    input_shape = x.get_shape().as_list()
-    
-    # only image dataset allowed: 4D tensor
-    assert len(input_shape)==4
-    
-    # build hybrid learning conv layer
-    with tf.variable_scope('hl_conv_1'):
-        hl_conv_1_embed = tf.constants
 
-def main():
-    # the feed-in layer
-    # using SHIP dataset
-    
+def main(args):
+    net = hl.hlconvnet()
+    net.offline_supervised('/home/hiroki/ships/raw_data/train/')
+    net.offline_unsupervised('/home/hiroki/ships/raw_data/unlabeled/')
+    net.train(1, 1, 1)
+
+tf.app.run()
 

tf_notes/tf_hlconv.py
@@ -168,21 +168,21 @@ def hlconv(
     fields_polarized = sig_polar(fields_stacked)
     for _i in xrange(kn):
         y[_i] = fields_polarized[_i]*arrivals[_i]
-    return (
-            tf.stack(y, axis=3, name=name+'-hlconv'),
-            make_unsupervised_train_op(
-                uw, 
-                patches, 
-                fields_polarized
-            )
+    y_stacked = tf.stack(y, axis=3, name=name+'-hlconv')
+    utrain_op, energy = make_unsupervised_train_op(
+            uw,
+            patches,
+            fields_polarized
     )
+    return (y_stacked, utrain_op, energy)
 
 # unsupervised optimizing policy for HLConv operator
 __global_learn_rate = 0.01
 # @uw               : variable uw
 # @patches          : tenosr patches
 # @fields_polarized : the polarized fields
-# @ret              : return an unsupervised training operator
+# @ret              : return an unsupervised training 
+#                     operator and an energy level op
 def make_unsupervised_train_op(
         uw,
         patches, 
@@ -201,6 +201,16 @@ def make_unsupervised_train_op(
     for _i in xrange(_n):
         grads[_i] = fields_reshaped[_i]*(patches-uw[_i])
     grads_stacked = tf.stack(grads, axis=0)
-    uw += __global_learn_rate*tf.reduce_sum(grads, axis=(1,2,3))
-    return uw # return updated uw as training operator
+    uw_updated = uw.assign(
+            uw + __global_learn_rate*\
+                    tf.reduce_sum(
+                        grads_stacked, 
+                        axis=(1,2,3)
+                    )
+                )
+    
+    # training energy is the maximum grad norm
+    energy = tf.reduce_max(tf.abs(grads_stacked))
+    
+    return (uw_updated, energy)
 

tf_notes/tf_hlconvnet.py
@@ -3,19 +3,33 @@
 # Algorithm and implementation are intrinscally authorized
 # by Chao Xiang
 
+# -*- coding:utf8 -*-
+
 import numpy as np
+import PIL.Image as pi
+import matplotlib.pyplot as pl
+
 import tensorflow as tf
 import tf_hlconv as hl
 
+import sys
+import os
+import time
+
 # configurations
-N_DIGITS = 8
-N, H, W, C = 1, 256, 256, 3
+N_DIGITS = 28
+N, H, W, C = 8, 256, 256, 3
 
 class hlconvnet(object):
     def __init__(self):
         # a batch of images [nbatch, height, width, channel]
         self.graph = tf.Graph()
-        # how to run unsupervised training and the supervised
+        self.saver = None
+        self.sess = None
+        self.summ = None
+        self.learning_rate = 0.01
+        self.stop_level = 0.01
+
         with self.graph.as_default():
             # data feed in and back
             self.x = tf.placeholder(
@@ -24,7 +38,7 @@ class hlconvnet(object):
             )
             self.y = tf.placeholder(
                     dtype=tf.int32,
-                    shape=[N, 1]
+                    shape=[N]
             )
 
             # containers for layers and parameters
@@ -34,14 +48,15 @@ class hlconvnet(object):
             self.sbs = [] # supervised biases
             self.uts = [] # unsupervised training ops
             self.layers = [] # layers 
+            self.energies = [] # unsupervised energies
 
 ##################### LAYER #1 ########################
 
             # generate the first hlconv layer
-            # with kernel size 2x2,
-            # with kernel number 256
+            # with kernel size 3x3
+            # with kernel number 16
             ksizes = [1,3,3,1]
-            n = 256
+            n = 16
             
             uw,sw = hl.hlconv_make_params(
                     n = n,
@@ -52,7 +67,7 @@ class hlconvnet(object):
             self.uws.append(uw)
             self.sws.append(sw)
             
-            hlconv, utrain_op = hl.hlconv(
+            hlconv, utrain_op, energy = hl.hlconv(
                     x = self.x,
                     uw = uw,
                     sw = sw,
@@ -64,11 +79,12 @@ class hlconvnet(object):
             
             self.hlc.append(hlconv)
             self.uts.append(utrain_op)
+            self.energies.append(energy)
 
             # add bias
             bias = tf.Variable(
                     tf.random_uniform(
-                        [hlconv.shape.as_list()[0]]
+                        [hlconv.shape.as_list()[3]]
                     )
             )
             add_bias = tf.nn.bias_add(
@@ -98,7 +114,7 @@ class hlconvnet(object):
             self.uws.append(uw)
             self.sws.append(sw)
 
-            hlconv, utrain_op = hl.hlconv(
+            hlconv, utrain_op, energy = hl.hlconv(
                     x = apply_act,
                     uw = uw,
                     sw = sw,
@@ -110,15 +126,19 @@ class hlconvnet(object):
 
             self.hlc.append(hlconv)
             self.uts.append(utrain_op)
+            self.energies.append(energy)
 
             # add bias
             bias = tf.Variable(
                     tf.random_uniform(
-                        [hlconv.shape.as_list()[0]]
+                        [hlconv.shape.as_list()[3]]
                     )
             )
             self.sbs.append(bias)
-            add_bias = tf.nn.bias_add(bias)
+            add_bias = tf.nn.bias_add(
+                    hlconv,
+                    bias
+            )
             
             # apply activation function
             apply_act = tf.nn.relu(add_bias)
@@ -134,21 +154,21 @@ class hlconvnet(object):
             ]
             self.logits = tf.nn.max_pool(
                     apply_act,
-                    ksizes = ksizes,
+                    ksizes,
                     strides = ksizes,
                     padding = 'VALID'
             )
-            self.logits = tf.reshape(self.logits, [N_DIGITS])
+            self.logits = tf.reshape(self.logits, [N, N_DIGITS])
 
 ################### PREDICTION AND TRAINING ##############
 
             # get predicted label and its probability
-            self.pred = tf.argmax(logits, 1)
-            self.prob = tf.nn.softmax(logits)
+            self.pred = tf.argmax(self.logits, 1)
+            self.prob = tf.nn.softmax(self.logits)
 
             # for training, convert from labels into one-hot vector
             self.onehot = tf.one_hot(
-                    tf.cast(labels, tf.int32), 
+                    tf.cast(self.y, tf.int32),
                     N_DIGITS,
                     1,
                     0
@@ -158,45 +178,182 @@ class hlconvnet(object):
             #self.loss = tf.reduce_sum(tf.square(self.y - feeds))
             self.loss = tf.losses.softmax_cross_entropy(
                     onehot_labels=self.onehot,
-                    logits=self.logtis
+                    logits=self.logits
             )
             
             # optimizer for supervised learning
-            self.opt = tf.train.AdagradOptimizer(learning_rate=0.01)
+            self.opt = tf.train.AdagradOptimizer(
+                    learning_rate = self.learning_rate
+            )
 
             # the only supervised training operator
             self.st = self.opt.minimize(self.loss)
+
+    ################# DATA PREPARATION ##################
+    # this will load all samples into memory for 
+    # supervised training, test, or prediction
+    # @path : path for data, it must be a dir containing 
+    #         subdirs; for training or test, samples with 
+    #         same labels have to be in same subdir.
+    def offline_supervised(self, path):
+        _subdirs = os.listdir(path)
+        self.labels = [] # unicode strings
+        self.sx_samples = []
+        self.sy_samples = []
+        
+        for _subdir in _subdirs:
+            self.labels.append(_subdir.decode('utf-8'))
+        
+        # to ensure an absolute mapping from id to label
+        self.labels.sort()
+
+        # check datasets
+        assert len(self.labels)==N_DIGITS
+        
+        for _i in xrange(len(self.labels)):
+            _label = self.labels[_i]
+            _path = os.path.join(path, _label)
+            _files = os.listdir(_path)
+            for _file in _files:
+                _im_path = os.path.join(
+                        _path, 
+                        _file.decode('utf-8')
+                )
+                _im = pi.open(_im_path)
+                _im = _im.resize([W,H])
+                self.sx_samples.append(np.array(_im))
+                self.sy_samples.append(_i)
+                #pl.imshow(_im)
+                #pl.show()
+                #break
+        # set up random generator(supervised)
+        self.svol = len(self.sx_samples) # volume of samples
+        self.sseq = np.arange(self.svol) # random sequence of samples
+        self.sidx = 0 # current index in sequence
+        
+        # check if data is enough for a batch
+        if self.svol < N:
+            raise NameError('Samples not enough for a batch!')
+
+        # construct supervised batch(x,y)
+        self.sbatch_x = np.zeros([N,H,W,C], np.float32)
+        self.sbatch_y = np.zeros([N], np.int32)
+
+    # Generate a batch of samples(x,y) to feed the network
+    def batch_supervised(self):
+        if self.sidx+N > self.svol: # rest samples not enough for a batch
+            np.random.shuffle(self.sseq)
+            self.sidx = 0
+
+        for _i in xrange(N):
+            _idx = self.sseq[self.sidx + _i]
+            self.sbatch_x[_i] = self.sx_samples[_idx]/255.0
+            self.sbatch_y[_i] = self.sy_samples[_idx]
+
+        self.sidx += N
+
+    # this will load all samples into memory for 
+    # unsupervised training and prediction
+    # @path : ensure only images in current path
+    def offline_unsupervised(self, path):
+        self.ux_samples = [] 
+        _files = os.listdir(path)
+        
+        for _file in _files:
+            _im_path = os.path.join(
+                    path, 
+                    _file.decode('utf-8')
+            )
+            _im = pi.open(_im_path)
+            _im = _im.resize([W,H])
+            self.ux_samples.append(np.array(_im))
+            #pl.imshow(_im)
+            #pl.show()
+            #break
+        
+        # set up random generator(unsupervised)
+        self.uvol = len(self.ux_samples) # volume of samples
+        self.useq = np.arange(self.uvol) # random sequence of samples
+        self.uidx = 0 # current index in sequence
+        
+        # check if data is enough for a batch
+        if self.uvol < N:
+            raise NameError('Samples not enough for a batch!')
+
+        # construct supervised batch(x)
+        self.ubatch_x = np.zeros([N,H,W,C], np.float32)
+
+    # Generate a batch of samples(x) to feed the network
+    def batch_unsupervised(self):
+        if self.uidx+N > self.uvol: # rest samples not enough for a batch
+            np.random.shuffle(self.useq)
+            self.uidx = 0
+
+        for _i in xrange(N):
+            _idx = self.useq[self.uidx + _i]
+            self.ubatch_x[_i] = self.ux_samples[_idx]/255.0
+
+        self.uidx += N
 
     # @nalt : number of alternation
     # @uepo : unsupervised iterations within an epoch
     # @sepo : supervised iterations within an epoch
     def train(self, nalt, uepo, sepo):
-        # initialization
-        self.sess.run(tf.global_variables_initializer())
-        for i in xrange(nalt):
-            for ui in xrange(uepo):
-                for uti in xrange(len(self.uts)):
+        print 'Training begin...'
+        path = time.strftime('MODEL_%Y%m%d_%H%M%S')
+        os.mkdir(path)
+        print 'models saved in ', path
+
+        with self.graph.as_default():
+            if self.saver==None:
+                self.saver = tf.train.Saver()
+            if self.sess==None:
+                self.sess = tf.Session()
+
+            # initialization
+            self.sess.run(tf.global_variables_initializer())
+            
+            for i in xrange(nalt):
+                # run unsupervised training epoch
+                for ui in xrange(uepo):
+                    self.batch_unsupervised()
                     self.sess.run(
-                            self.uts[uti], 
+                            self.uts,
                             feed_dict={
-                                self.x:,
-                                self.y:
-                                })
-            for si in xrange(sepo):
-                self.sess.run(
-                        self.st,
-                        feed_dict={
-                            self.x:,
-                            self.y:
-                        })
-            # for each alternation, print the training losses
-            if __debug__:
-                self.sess.run(self.loss) # need another op to calc
-                for uti 
-                self.sess.run(self.enery) # need a grou of ops
+                                self.x : self.ubatch_x
+                            }
+                    )
+                for si in xrange(sepo):
+                    self.batch_supervised()
+                    self.sess.run(
+                            self.st,
+                            feed_dict={
+                                self.x : self.sbatch_x,
+                                self.y : self.sbatch_y
+                            }
+                    )
+                # for each alternation, print the training losses
+                # and the unsupervised energy level
+                if __debug__:
+                    print 'Training loss:', self.sess.run(
+                            self.loss, 
+                            feed_dict={
+                                self.x : self.sbatch_x,
+                                self.y : self.sbatch_y
+                            })
+                    print 'Unsupervised energies:', self.sess.run(
+                            self.energies, 
+                            feed_dict={
+                                self.x : self.ubatch_x
+                            })
+                # every 100 steps to save a copy of the model
+                if i%100 == 0 or i==nalt-1:
+                    _model = os.path.join(path, 'hlconvnet')
+                    self.saver.save(self.sess, _model, global_step=i)
+    
+    # @model : the trained model path
+    def predict(self, model):
+        self.saver = tf.Saver()
+        with self.graph.as_default():
+            print 'hello'
 
-# test for class
-def main(args):
-    net = hlconvnet()
-
-tf.app.run()